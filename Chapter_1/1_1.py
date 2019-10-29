ef2b039ca47b811d53572f4b273e6084498d9d92
Gabriel Alexander Mongalo <gmongalo90@gmail.com>
2019-10-28 22:32:56 -0700

    rounded down mid

--- Chapter_1/1_1.py
+++ Chapter_1/1_1.py
@@ -1,16 +1,17 @@
+from typing import List, Union
+
 # What is the max n-steps it will take to get through a sorted list of 128 items
 # using binary_search.
 
-def binary_search(random_num_list, item):
+def binary_search(num_list: List, item: int) -> Union[int, None]:
     low = 0
     high = len(num_list)-1
 
     counter = 0
 
     while low <= high:
-        mid = (low + high) / 2
+        mid = (low + high) // 2
         guess = num_list[mid]
-        counter += 1
 
         if guess == item:
             return mid
@@ -20,13 +21,14 @@ def binary_search(random_num_list, item):
             low = mid + 1
         else:
             return None
+
+        counter += 1
+
     return counter
 
 
-num_list = list(range(0,128))
+num_list = range(0,127)
+num_list_doubled = range(0,255)
 
-print(num_list)
-print('List size is' + ' '+ str(len(num_list)))
-
-print(binary_search(num_list, num_list[-1:]))
-print(binary_search(num_list*2, num_list[-1:]))
+print(f'''Max number of steps for 128 items {binary_search(num_list, 127)}''')
+print(f'''Max number of steps for 256 items {binary_search(num_list_doubled, 255)}''')
